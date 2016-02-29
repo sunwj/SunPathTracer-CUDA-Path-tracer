c52dc27f214506ab4b8cea2fc233f6fbc0f40fc3
WanjieSun <wanjie.sun@qq.com>
2016-02-29 23:01:10 +0800

    add constructor

--- cuda_camera.h
+++ cuda_camera.h
@@ -7,11 +7,31 @@
 
 #include <cuda_runtime.h>
 
+#include "cuda_ray.h"
+
 class cudaCamera
 {
 public:
+    __host__ __device__ cudaCamera(const float3& _pos, const float3& _u, const float3& _v, const float3& _w, unsigned int _filmW,
+                                   unsigned int _filmH)
+    {
+        pos = pos;
+        u = _u;
+        v = _v;
+        w = _w;
+        filmW = _filmW;
+        filmH = _filmH;
+    }
+
+    __device__ void GenerateRay(unsigned int x, unsigned int y, cudaRay* ray)
+    {
+
+    }
+
 public:
-    __device__ float3 pos;
+    unsigned int filmW, filmH;
+    float3 pos;
+    float3 u, v, w;
 };
 
 #endif //SUNPATHTRACER_CAMERA_H
