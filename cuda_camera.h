23274c706d15ee7cbfcf7dd5053e42131cb9de49
WanjieSun <wanjie.sun@qq.com>
2016-03-03 08:29:04 +0800

    add generate ray

--- cuda_camera.h
+++ cuda_camera.h
@@ -6,30 +6,55 @@
 #define SUNPATHTRACER_CAMERA_H
 
 #include <cuda_runtime.h>
+#include <curand_kernel.h>
 
+#include "helper_math.h"
 #include "cuda_ray.h"
 
 class cudaCamera
 {
 public:
-    __host__ __device__ cudaCamera(const float3& _pos, const float3& _u, const float3& _v, const float3& _w, unsigned int _filmW,
-                                   unsigned int _filmH)
+    __host__ __device__ cudaCamera(const float3& _pos, const float3& _u, const float3& _v, const float3& _w, float fov, unsigned int _imageW,
+                                   unsigned int _imageH)
     {
         pos = pos;
         u = _u;
         v = _v;
         w = _w;
-        filmW = _filmW;
-        filmH = _filmH;
+        imageW = _imageW;
+        imageH = _imageH;
+        aspectRatio = (float)imageW / (float)imageH;
+        tanFovOverTwo = tanf(fov * 0.5f * M_PI / 180.f);
     }
 
-    __device__ void GenerateRay(unsigned int x, unsigned int y, cudaRay* ray)
+    __host__ __device__ cudaCamera(const float3& pos, const float3& target, const float3& up, float fov, unsigned int _imageW, unsigned int _imageH)
     {
+        pos = pos;
+        w = normalize(pos - target);
+        u = cross(v, w);
+        v = cross(w, u);
+        imageW = _imageW;
+        imageH = _imageH;
+        aspectRatio = (float)imageW / (float)imageH;
+        tanFovOverTwo = tanf(fov * 0.5f * M_PI / 180.f);
+    }
 
+    __device__ void GenerateRay(unsigned int x, unsigned int y, curandState& rng, cudaRay* ray)
+    {
+        float nx = 2.f * ((x + curand_uniform(rng)) / imageW) - 1.f;
+        float ny = 2.f * ((y + curand_uniform(rng)) / imageH) - 1.f;
+
+        nx = nx * aspectRatio * tanFovOverTwo;
+        ny = ny * tanFovOverTwo;
+
+        ray->orig = pos;
+        ray->dir = normalize(nx * u + ny * v - w);
     }
 
 public:
-    unsigned int filmW, filmH;
+    unsigned int imageW, imageH;
+    float aspectRatio;
+    float tanFovOverTwo;
     float3 pos;
     float3 u, v, w;
 };
