9772645ba4b2185b5eace7c46f7307a8c6e25541
WanjieSun <wanjie.sun@qq.com>
2016-03-31 21:45:43 +0800

    fix sphere intersection

--- cuda_shape.h
+++ cuda_shape.h
@@ -27,22 +27,28 @@ public:
     __device__ virtual bool Intersect(const cudaRay& ray, float* t) const
     {
         float3 L = ray.orig - center;
-        float a = dot(ray.orig, ray.dir);
+        //float a = dot(ray.dir, ray.dir);
+        // ray.dir is normalized, so dir dot dir is cos(0) = 1
+        float a = 1.f;
         float b = 2.f * dot(ray.dir, L);
         float c = dot(L, L) - radius * radius;
 
         float discr = b * b - 4.f * a * c;
-        if(discr < 0)
-            return false;
-        else
+        if(discr > 0.f)
         {
-            float q = (b > 0.f) ? (-0.5f * (b + sqrtf(discr))) : (-0.5f * (b - sqrtf(discr)));
-            float t1 = q / a;
-            float t2 = c / q;
-            *t = (t1 > t2) ? t1 : t2;
+            discr = sqrtf(discr);
+            *t = (-b - discr) / (2.f * a);
+
+            // check valid interval
+            if(*t < 0.f)
+                t = (-b + discr) / (2.f * a);
+            if(*t < 0.f || t > (FLT_MAX - 1.f))
+                return false;
 
             return true;
         }
+
+        return false;
     }
 
 private:
