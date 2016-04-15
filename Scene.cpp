eeb81d59c660825846ac240d7a0eed4cba71243e
WanjieSun <wanjie.sun@qq.com>
2016-04-15 21:53:15 +0800

    fixed

--- Scene.cpp
+++ Scene.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "Scene.h"
+#include "helper_cuda.h"
 
 Scene::Scene()
 {
@@ -12,4 +13,33 @@ Scene::Scene()
 Scene::~Scene()
 {
 
+}
+
+void Scene::BuildSceneForGPU(cudaScene &scene)
+{
+    //copy camera
+    scene.camera = this->camera;
+
+    //copy materials
+    scene.num_materials = this->materials.size();
+    if(scene.num_materials != 0)
+    {
+        checkCudaErrors(cudaMalloc((void**)&(scene.materials), sizeof(cudaMaterial) * this->materials.size()));
+        checkCudaErrors(cudaMemcpy(scene.materials, this->materials.data(), sizeof(cudaMaterial) * this->materials.size(), cudaMemcpyHostToDevice));
+    }
+
+    //copy geometries
+    scene.num_spheres = this->spheres.size();
+    if(scene.num_spheres != 0)
+    {
+        checkCudaErrors(cudaMalloc((void**)&(scene.spheres), sizeof(cudaSphere) * this->spheres.size()));
+        checkCudaErrors(cudaMemcpy(scene.spheres, this->spheres.data(), sizeof(cudaSphere) * this->spheres.size(), cudaMemcpyHostToDevice));
+    }
+
+    scene.num_aabb_boxes = this->aabb_boxes.size();
+    if(scene.num_aabb_boxes != 0)
+    {
+        checkCudaErrors(cudaMalloc((void**)&(scene.aabb_boxes), sizeof(cudaAABB) * this->aabb_boxes.size()));
+        checkCudaErrors(cudaMemcpy(scene.aabb_boxes, this->aabb_boxes.data(), sizeof(cudaAABB) * this->aabb_boxes.size(), cudaMemcpyHostToDevice));
+    }
 }
--- Scene.h
+++ Scene.h
@@ -8,7 +8,10 @@
 #include <vector>
 #include <cuda_runtime.h>
 
+#include "cuda_material.h"
+#include "cuda_camera.h"
 #include "cuda_shape.h"
+#include "cuda_scene.h"
 
 class Scene
 {
@@ -16,7 +19,39 @@ public:
     Scene();
     ~Scene();
 
+    void AddCamera(const cudaCamera& camera)
+    {
+        this->camera = camera;
+    }
+
+    void AddMaterial(const cudaMaterial& material)
+    {
+        this->materials.push_back(material);
+    }
+
+    void AddSphere(const cudaSphere& sphere)
+    {
+        this->spheres.push_back(sphere);
+    }
+
+    void AddAABB(const cudaAABB& aabb)
+    {
+        this->aabb_boxes.push_back(aabb);
+    }
+
+    unsigned int GetLastMaterialID(void) {return this->materials.size() - 1;}
+
+    //todo: add build scene
+    void BuildSceneForGPU(cudaScene& scene);
+
 public:
+    //camera
+    cudaCamera camera;
+
+    //material
+    std::vector<cudaMaterial> materials;
+
+    //geometries
     std::vector<cudaSphere> spheres;
     std::vector<cudaAABB> aabb_boxes;
 };
